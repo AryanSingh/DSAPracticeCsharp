19214f132ee48f34fbc3bbf77a49ce842f435652
Aryan Singh <aryan.singh@skypointcloud.com>
2023-08-10 20:45:07 +0530

    calculrate max profit

---
+++ DSAPracticeCsharp/CalculateMaxProfit.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+
+ 
+int calculateMaxProfit(List<int> prices)
+        {
+            int minTillNow = int.MaxValue;
+            int maxProfit = 0;
+            prices.ForEach(delegate(int i)
+            {
+                if (i < minTillNow)
+                {
+                    minTillNow = i;
+                }
+
+                if (i - minTillNow > maxProfit)
+                {
+                    maxProfit = i - minTillNow;
+                }
+            });
+            return maxProfit;
+        }
+
+
+Console.WriteLine(calculateMaxProfit(new List<int> { 310, 315, 275, 295, 260, 270, 290, 230, 255, 250 }));
